175204c0ba00b235f8c39f0c7ed6b797dc86524f
AenishShrestha <84612270+AenishShrestha@users.noreply.github.com>
2023-04-10 16:10:47 +0545

    Update app.py

--- app.py
+++ app.py
@@ -74,82 +74,79 @@ with streamlit_analytics.track():
     }
     
     try:
-        @st.cache(persist=True, allow_output_mutation=True)
-        def news():
-            response = requests.get(url, headers=headers)
-            html = response.content
+        response = requests.get(url, headers=headers)
+        html = response.content
 
-            soup = BeautifulSoup(html, "html.parser")
-            text = soup.get_text()
-            with open("matches.txt", "w",encoding="utf-8") as f:
-                f.write(text)
+        soup = BeautifulSoup(html, "html.parser")
+        text = soup.get_text()
+        with open("matches.txt", "w",encoding="utf-8") as f:
+            f.write(text)
 
 
 
-            # Convert the text content to markdown
-            markdown_text = markdown.markdown(text)
-            text = markdown_text.replace('\n','')
-             #slicing the characters
-            if url == "https://www.futuretools.io/news":
-                limited_text = text[0:3000]
+        # Convert the text content to markdown
+        markdown_text = markdown.markdown(text)
+        text = markdown_text.replace('\n','')
+         #slicing the characters
+        if url == "https://www.futuretools.io/news":
+            limited_text = text[0:3000]
 
-            else : 
-                limited_text = text[500:5000]
-            # limited_text = text[0:8000]
-            text_bytes = limited_text.encode("utf-8")
-            text_splitter = RecursiveCharacterTextSplitter( 
-                        chunk_size = 800,
-                        chunk_overlap  = 20
-                    )
-            texts = text_splitter.split_text(limited_text)
-    #         st.warning(len(texts))
+        else : 
+            limited_text = text[500:5000]
+        # limited_text = text[0:8000]
+        text_bytes = limited_text.encode("utf-8")
+        text_splitter = RecursiveCharacterTextSplitter( 
+                    chunk_size = 800,
+                    chunk_overlap  = 20
+                )
+        texts = text_splitter.split_text(limited_text)
+#         st.warning(len(texts))
 
 
-            #openai 
-            OPENAI_API_KEY =  st.secrets["OPEN_API_KEY"]
-            # Convert to desired format
-            time_in_desired_format = current_time.strftime("%d %b %Y, %H:%M:%S")
-            embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+        #openai 
+        OPENAI_API_KEY =  st.secrets["OPEN_API_KEY"]
+        # Convert to desired format
+        time_in_desired_format = current_time.strftime("%d %b %Y, %H:%M:%S")
+        embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
-            if url == "https://www.futuretools.io/news":
-                prefix_messages = [
-                    {"role": "system", "content": "You are a helpful AI Website Explainer."},
-                    {"role": "user", "content": f"Create long form summary.Give Me Summarizes based on last 3 days from todays date. Notice today's date = {time_in_desired_format}.Maintain the order of the article with sorting of descending with respect to today's date. Output as a markdown.Do you understand?"},
-                    {"role": "assistant", "content": "Yes i understood.I will not break the character and consider today's date to give only 3 days "},]
-
-                query = 'Create long form summary of last 3 days from todays date : {time_in_desired_format}. List the bulletpoints. Give the headline As "LATEST AI NEWS" in h1 tag .Also notice to provide results of last 3 days. Output as a markdown.Do not repeat yourself.'
-
-            else:
-               prefix_messages = [
+        if url == "https://www.futuretools.io/news":
+            prefix_messages = [
                 {"role": "system", "content": "You are a helpful AI Website Explainer."},
-                {"role": "user", "content": "Create long form summary. Output as a markdown."},
-                {"role": "assistant", "content": "Okay what is the name of website?"},]
-               query = 'Create long form summary. Only List the bulletpoints. Give the headline in h1 tag. Output as a markdown.Do not repeat yourself.'
+                {"role": "user", "content": f"Create long form summary.Give Me Summarizes based on last 3 days from todays date. Notice today's date = {time_in_desired_format}.Maintain the order of the article with sorting of descending with respect to today's date. Output as a markdown.Do you understand?"},
+                {"role": "assistant", "content": "Yes i understood.I will not break the character and consider today's date to give only 3 days "},]
+
+            query = 'Create long form summary of last 3 days from todays date : {time_in_desired_format}. List the bulletpoints. Give the headline As "LATEST AI NEWS" in h1 tag .Also notice to provide results of last 3 days. Output as a markdown.Do not repeat yourself.'
+
+        else:
+           prefix_messages = [
+            {"role": "system", "content": "You are a helpful AI Website Explainer."},
+            {"role": "user", "content": "Create long form summary. Output as a markdown."},
+            {"role": "assistant", "content": "Okay what is the name of website?"},]
+           query = 'Create long form summary. Only List the bulletpoints. Give the headline in h1 tag. Output as a markdown.Do not repeat yourself.'
 
 
 
-            with open("foo.pkl", 'wb') as f:
-                    pickle.dump(embeddings, f)
+        with open("foo.pkl", 'wb') as f:
+                pickle.dump(embeddings, f)
 
-            with open("foo.pkl", 'rb') as f:
-                new_docsearch = pickle.load(f)
+        with open("foo.pkl", 'rb') as f:
+            new_docsearch = pickle.load(f)
 
-            docsearch = FAISS.from_texts(texts, new_docsearch)
+        docsearch = FAISS.from_texts(texts, new_docsearch)
 
-            docs = docsearch.similarity_search(query)
+        docs = docsearch.similarity_search(query)
 
-            chain = load_qa_chain(OpenAIChat(openai_api_key=OPENAI_API_KEY,temperature=0,prefix_messages=prefix_messages), chain_type="stuff")
+        chain = load_qa_chain(OpenAIChat(openai_api_key=OPENAI_API_KEY,temperature=0,prefix_messages=prefix_messages), chain_type="stuff")
 
-            result = chain.run(input_documents=docs, question=query)
-            result = result.replace("\n\n--","").replace("\n--","").strip()
+        result = chain.run(input_documents=docs, question=query)
+        result = result.replace("\n\n--","").replace("\n--","").strip()
 
-            # Convert to desired format
-            # time_in_desired_format = current_time.strftime("%d %b %Y, %H:%M:%S")
+        # Convert to desired format
+        # time_in_desired_format = current_time.strftime("%d %b %Y, %H:%M:%S")
 
-            # Display current date and time in desired format
-            st.write('Current Date & Time:', time_in_desired_format)
-            st.markdown(result)
-        news()
+        # Display current date and time in desired format
+        st.write('Current Date & Time:', time_in_desired_format)
+        st.markdown(result)
         st.markdown("Made with ❤️ by [Aenish Shrestha](https://twitter.com/aenish_shrestha)")
         components.iframe("https://aenishshrestha.substack.com/embed",height=500)
     except Exception as e :
